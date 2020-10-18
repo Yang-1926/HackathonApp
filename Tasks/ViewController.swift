9c9f20ac87719aee3761e871692c09fb5aab2bec
Andrew <wlaurencevk@gmail.com>
2020-10-17 19:41:56 -0700

    just following the tutorial

--- Tasks/ViewController.swift
+++ Tasks/ViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Get all current saved tasks
+        
     }
 
 
@@ -30,5 +33,18 @@ extension ViewController: UITableViewDelegate {
 
 
 extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
     
 }
